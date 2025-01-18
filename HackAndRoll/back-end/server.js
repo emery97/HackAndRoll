c101f2847064aece8f73a4a9962704514d308df6
enjia <enjia.wu@gmail.com>
2025-01-18 12:45:40 +0800

    updated ui

--- HackAndRoll/back-end/server.js
+++ HackAndRoll/back-end/server.js
@@ -1,13 +1,4 @@
-const express = require('express');
-const { runTest, runSelectedTest } = require('./testController');
-const signIn = require('./signInController');
-const getTestCaseStatus = require('./testCaseStatusController');
-const getTestCaseStatusByBrowser = require('./statusByBrowserController');
-const getTestCases = require('./testCasesControllerv2');
-const getAllTestCases = require('./alltestCasesController')
-const testCaseUpdate = require('./testCaseUpdateController');
-const noCodeTestCase = require('./noCodeTestCase');
-const noCodeTestCaseImprovement = require('./noCodeTestCaseImprovement');
+const express = require('express');yield
 const cors = require('cors');
 
 const app = express();
@@ -15,17 +6,6 @@ const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
-app.get('/testcasestatus', getTestCaseStatus);
-app.get('/statusbybrowser', getTestCaseStatusByBrowser);
-app.get('/testcases', getTestCases);
-app.get('/alltestcases', getAllTestCases);
-app.post('/signin', signIn);
-app.post('/run-test', runTest);
-app.post('/run-selected-test', runSelectedTest);
-app.post('/generatetestcase', noCodeTestCase);
-app.post("/improvetestcase", noCodeTestCaseImprovement);
-
-testCaseUpdate();
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
