5c6f596d8f161f19f2ed36c177d927e89a5d6a7f
braydenNP <s10258659@connect.np.edu.sg>
2025-01-18 14:51:23 +0800

    mongo.db database access

---
+++ HackAndRoll/back-end/ClothingItem.js
@@ -0,0 +1,16 @@
+// ClothingItem.js (Backend)
+class ClothingItem {
+    constructor(id, type, name, color, formal, temperatureRange, lastWorn, attributes = {}) {
+      this.id = id; // Unique identifier
+      this.type = type; // 'top', 'bottom', 'outerwear', 'accessory'
+      this.name = name; // e.g., 'Blue T-Shirt'
+      this.color = color; // e.g., 'blue'
+      this.formal = formal; // e.g., false
+      this.temperatureRange = temperatureRange; // { min: 15, max: 30 }
+      this.lastWorn = lastWorn; // Date object or timestamp
+      this.attributes = attributes; // Category-specific attributes
+    }
+  }
+  
+  module.exports = ClothingItem;
+  
